5525bd63a532bf145a54bcfb8ae3db977b766feb
root <root@ip-172-31-45-150.ap-south-1.compute.internal>
2021-06-06 18:12:36 +0000

    added bump POM Version script

---
+++ bump_pom_version.sh
@@ -0,0 +1,28 @@
+ #!/bin/bash
+
+ function bumpversion(){
+        CURRENT_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | egrep '^[0-9\.]*$'`
+        local VERSION_VALUE=`echo ${CURRENT_VERSION} | sed -e 's/[^0-9][^0-9]*$//'`
+        local CURRENT_BUILD_NUMBER=`echo ${VERSION_VALUE} | sed -e 's/[0-9]*\.//g'`
+        local NEXT_BUILD_NUMBER=`expr ${CURRENT_BUILD_NUMBER} + 1`
+        NEXT_PROJECT_VERSION=`echo ${CURRENT_VERSION} | sed -e "s/[0-9][0-9]*\([^0-9]*\)$/${NEXT_BUILD_NUMBER}/"`
+ }
+
+ function updatePomToNextVersion() {
+         echo "Updating project version to [${NEXT_PROJECT_VERSION}]..."
+         mvn org.codehaus.mojo:versions-maven-plugin:1.3.1:set -DgenerateBackupPoms=false -DnewVersion=${NEXT_PROJECT_VERSION}
+ }
+
+ function pushpomversionchanges() {
+        echo "Preparing updated files for commit..."
+        git status
+        git add .
+        echo "Committing changes..."
+        local COMMIT_MESSAGE="Auto commit from CI - incremented build number from [${CURRENT_VERSION}] to [${NEXT_PROJECT_VERSION}]."
+        git commit -m "${COMMIT_MESSAGE}"
+        echo "Pushing changes to origin..."
+        git push origin
+ }
+bumpversion
+updatePomToNextVersion
+pushpomversionchanges
